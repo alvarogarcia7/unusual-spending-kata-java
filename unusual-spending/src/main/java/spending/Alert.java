61e5800434a7de4de20d9571d3886517a82aa185
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-09-16 19:58:41 +0200

    Refactor: move responsibility to Email Factory

--- unusual-spending/src/main/java/spending/Alert.java
+++ unusual-spending/src/main/java/spending/Alert.java
@@ -1,16 +1,13 @@
 package spending;
 
 public class Alert {
+	private final String text;
+
+	public Alert(String text) {
+		this.text = text;
+	}
+
 	public String getText() {
-		return "Hello card user!\n" +
-			"\n" +
-			"We have detected unusually high spending on your card in these categories:\n" +
-			"\n" +
-			"* You spent $148 on groceries\n" +
-			"* You spent $928 on travel\n" +
-			"\n" +
-			"Love,\n" +
-			"\n" +
-			"The Credit Card Company";
+		return text;
 	}
 }
--- unusual-spending/src/main/java/spending/EmailFactory.java
+++ unusual-spending/src/main/java/spending/EmailFactory.java
@@ -2,7 +2,16 @@ package spending;
 
 public class EmailFactory {
 	public void aNew(UnusualSpendingReport unusualSpendingReport) {
-		send(new Alert());
+		send(new Alert("Hello card user!\n" +
+			"\n" +
+			"We have detected unusually high spending on your card in these categories:\n" +
+			"\n" +
+			"* You spent $148 on groceries\n" +
+			"* You spent $928 on travel\n" +
+			"\n" +
+			"Love,\n" +
+			"\n" +
+			"The Credit Card Company"));
 	}
 
 	protected void send(Alert alert) {
