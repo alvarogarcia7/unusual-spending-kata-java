3744c35577649c29f0f803a465d33cd912e33510
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-09-16 20:01:25 +0200

    Refactor: extract a StringBuffer

--- unusual-spending/src/main/java/spending/EmailFactory.java
+++ unusual-spending/src/main/java/spending/EmailFactory.java
@@ -2,16 +2,17 @@ package spending;
 
 public class EmailFactory {
 	public void aNew(UnusualSpendingReport unusualSpendingReport) {
-		send(new Alert("Hello card user!\n" +
+		final StringBuffer stringBuffer = new StringBuffer().append("Hello card user!\n" +
 			"\n" +
 			"We have detected unusually high spending on your card in these categories:\n" +
-			"\n" +
-			"* You spent $" + unusualSpendingReport.get(0).getValue() + " on " + unusualSpendingReport.get(0).getTitle() + "\n" +
+			"\n");
+		stringBuffer.append("* You spent $" + unusualSpendingReport.get(0).getValue() + " on " + unusualSpendingReport.get(0).getTitle() + "\n" +
 			"* You spent $" + unusualSpendingReport.get(1).getValue() + " on " + unusualSpendingReport.get(1).getTitle() + "\n" +
 			"\n" +
 			"Love,\n" +
 			"\n" +
-			"The Credit Card Company"));
+			"The Credit Card Company");
+		send(new Alert(stringBuffer.toString()));
 	}
 
 	protected void send(Alert alert) {
